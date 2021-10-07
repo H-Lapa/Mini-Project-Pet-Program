64d6a3c2d5a85a6cd0d7e23c2ca92f4fe2391403
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-10-07 21:14:44 +0100

    Level 2
    Using variables, assignments and expressions and at least one method that returns a result.

--- Main.java
+++ Main.java
@@ -1,23 +1,60 @@
 //Author: Hugo Lapa
 //07/10/2021
 import java.util.Scanner;
+import java.util.Random;
 
 //main class for the program
-class petProgram {
+class Main {
   //main function executed
-  public static void main(String[] args) {
-    petNameMessage();
+  public static void main(String[] args) 
+  {
+    sequence();
+    System.exit(1);
+  }
+
+  //method calls methods in the right order
+  public static void sequence()
+  {
+    String name = getPetName();
+    print(name);
+    int hunger = getHunger();
+    stateOfPet(hunger);
+
   }
 
   //produces message with pet name
-  public static void petNameMessage ()
+  public static String getPetName ()
   {
     Scanner scanner = new Scanner(System.in);
     
     System.out.print("What's your pets name? ");
     String name = scanner.nextLine(); // input for pets name
 
-    //message with name
-    System.out.print("Happy 0th Birthday " + name);
+    return name;
+  }
+
+  //outputs to the screen
+  public static void print (String txt) 
+  {
+    System.out.println(txt);
+    return;
+  }
+
+  //outputs detail about the state of the pet
+  public static void stateOfPet (int hunger)
+  {
+    print("Hunger level is " + hunger + "/5");
+    return;
+  }
+
+  //produces a random number between 1 and 5 to be a hunger value
+  public static int getHunger ()
+  {
+    Random rand = new Random();
+    int hunger;
+
+    hunger = rand.nextInt(5) + 1;
+
+    return hunger;
   }
 }
