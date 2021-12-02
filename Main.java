1007935e6d975ff8198457cd20410321bc33a168
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-12-02 16:49:59 +0000

    Comments

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 //Author: Hugo Lapa
 //31/10/2021
+
+//impots for the program
 import java.util.Scanner;
 import java.util.Random;
+
+
 class Game {
     public static void main (String[] args)
     {
+        //declaration of variables for creating pets
         String petname;
         String petspecies;
         general.print("(maximum of 5)");
         int petQuant = general.getint("Quantity of pets: ");
-        pet[] petarr = new pet[petQuant];
+        pet[] petarr = new pet[petQuant]; // pet array delcaration
+
+        //loop which creates pets, and puts them into an array
         for (int i = 0; i < petQuant; i++)
         {
             petname = general.getstring("Pet name: ");
@@ -18,6 +25,8 @@ class Game {
             petarr[i] = record;
         }
 
+
+        //declaration of variables needed for while loop
         boolean end = false;
         int count = 0;
         String action;
@@ -26,6 +35,7 @@ class Game {
         int[][] temp = new int[5][2];
         pet[] sortedArr = new pet[petQuant];
 
+        //while loop until the end of game becomes true
         while (end == false)
         {
             //sets temp to current petarray stored, clones it otherwise temp will be assigned to memory location and update along with petarr, used later for comparison
@@ -34,23 +44,24 @@ class Game {
             //implement insertion sort for new array
             sortedArr = insertionSort(petarr);
 
-
             //prints pet name and stats to screen
             for (int z = 0; z < petQuant; z++)
             {
-                //this hsould loop through the insertion sorted array
+                //this should loop through the insertion sorted array
                 general.print(" ");
                 general.print(z+1 + ". " + getname(sortedArr[z]) + " the " + getspecies(sortedArr[z]));
                 stateofpet(sortedArr[z]);
             }
 
             System.out.println("             "); // gap to make visually appealing
+
+            //user interaction, asking which pet to apply action and wether to feed or cuddle
             num = general.getint("Pick a pet to apply action: ");
             petnum = num-1;
-            //petnum = findloc(petarr, sortedArr[petnum]);
+            petnum = findloc(petarr, sortedArr[petnum]);
             action = general.getstring("Feed or Cuddle pet: ");
 
-            // if function returns true
+            // if function returns true, function also acts on the pet which was selected by feeding or cuddling
             if (feedCuddle(action, petarr[petnum]))
             {
                 for (int i = 0; i < petQuant;i++)
@@ -103,36 +114,44 @@ class Game {
         }
 
         System.exit(0);
-    }
+    }//END of main
 
+    //clones the hunger and happiness stats from a pet class into a 2d array and returns it
     public static int[][] statClone (pet[] curr)
     {
         int[][] petStat = new int[5][2];
         for (int i=0; i< curr.length; i++)
         {
+            //copying the hunger and happiness values
             petStat[i][0] = curr[i].hunger;
             petStat[i][1] = curr[i].happiness;
         }
         return petStat;
-    }
+    }//END of statClone
 
+    //checks if the user has lost in the game
     public static boolean loseCheck (int[][] prev, pet[] curr)
     {
+        //this loops through the size of the array
         for (int z=0; z< curr.length; z++)
         {
+            //if the hunger or happiness is equal to 5 from previous and current round then the user has lost
             if ((prev[z][0] == 5 && curr[z].hunger == 5) || (prev[z][1] == 5 && curr[z].happiness == 5) )
             {
+                //thus returns true
                 return true;
             }
         }
 
+        //if hasnt lost will return false
         return false;
-    }
+    }//END of loseCheck
 
+    //Does insertion sort on the pet array
     public static pet[] insertionSort (pet[] array)
     {
-        //does insertion sort on pet array and returns
         int n = array.length;
+        //loops through array
         for (int j = 1; j < n; j++) {
             int key = getCombinedStats(array[j]);
             pet secKey = array[j];
@@ -144,58 +163,42 @@ class Game {
             array[i+1] = secKey;
         }
         return array;
-    }
-
-
-
+    }//END of insertionSort
+    
+    //returns the addition of both happiness and hunger
     public static int getCombinedStats (pet x)
     {
+        
         int num = gethappiness(x) + gethunger(x);
         return num;
-    }
+    }//END of getCombinedStats
 
-
-
-
-
-
-
-
-
-
-
-
-    //accessor methods
-    public static void setpetspecies (pet p, String txt)
+    //finds the position of a specfic pet in an array
+    public static int findloc (pet[] petarr, pet petchoice)
     {
-        p.species = txt;
-    }
-    public static void setpetname (pet p, String txt)
-    {
-        p.name = txt;
-    }
-    public static String getname (pet p)
-    {
-        return p.name;
-    }
-    public static String getspecies (pet p)
-    {
-        return p.species;
-    }
-    public static int gethunger (pet p)
-    {
-        return p.hunger;
-    }
-    public static int gethappiness (pet p)
-    {
-        return p.happiness;
-    }
+        //loops through the pet array
+        for (int i = 0; i < petarr.length; i++)
+        {
+            //if the pet in the array is the same as the pet your searching for
+            if (petarr[i] == petchoice)
+            {
+                //then return the i value 
+                return i;
+            }
+        }
+
+        return 0;
+    }//END of findloc
+
+    //gives a pet random hunger and happiness levels
     public static pet randStats (pet record)
     {
         record.hunger = general.getRand(5);
         record.happiness = general.getRand(5);
         return record;
-    }
+    }//END of randStats
+    
+    //returns the state fo the pet depedning on the value of hunger / happiness
     public static void stateofpet (pet record)
     {
         //hunger value then used to determine message
@@ -222,44 +225,45 @@ class Game {
         } else {
             general.print("Pet is depressed. Happiness score at " + gethappiness(record) +"/5");
         }
-    }
+    }//END of stateofpet
+    
+    //applies the cuddle or feed, on a chosen pet 
     public static boolean feedCuddle (String action, pet record)
     {
+        //if its cuddle, will decrease the value of happiness
         if (action.equals("cuddle")) {
             record.happiness = (gethappiness(record) - general.getRand(3));
+            //if happiness is less than 1 then it becomes 1
             if (gethappiness(record) < 1) {
                 record.happiness = 1;
             }
+            //however hunger value increases with a limit of 5
             record.hunger = (gethunger(record) + general.getRand(3));
             if (gethunger(record) > 5) {
                 record.hunger = 5;
             }
             return true;
         }
-
+        
+        //if its feed, will decrease the value of hunger
         if (action.equals("feed")) {
             record.hunger = (gethunger(record) - general.getRand(3));
+            //hunger is less than 1 it gets set back to 1 as thats the limit
             if (gethunger(record) < 1) {
                 record.hunger = 1;
             }
+            //however the happiness value increases with a limit of 5
             record.happiness = (gethappiness(record) + general.getRand(3));
             if (gethappiness(record) > 5) {
                 record.happiness = 5;
             }
             return true;
         }
+        //returns false when none of the statements was successful
         return false;
-    }
-    public static void setpethappiness (pet p, int newvalue)
-    {
-        p.happiness = newvalue;
-        return;
-    }
-    public static void setpethunger (pet p, int newvalue)
-    {
-        p.hunger = newvalue;
-        return;
-    }
+    }//END of feedCuddle
+    
+    //worsening of the hunger and happiness of pets randomly
     public static void regressStats (pet record)
     {
         int newhunger = gethunger(record) + general.getRand(2);
@@ -274,7 +278,12 @@ class Game {
         }
         record.hunger = newhunger;
         record.happiness = newhappiness;
-    }
+    }//END of regressStats
+    
+
+    //accessor methods, only methods directly accessing the values in the pet ADT
+    
+    //creates an ADT using the parameters
     public static pet createpet (String name, String species)
     {
         pet record = new pet();
@@ -282,10 +291,53 @@ class Game {
         setpetspecies(record, species);
         randStats(record);
         return record;
-    }
+    }//END of createpet
+    
+    //getters and setters
+    
+    public static void setpetspecies (pet p, String txt)
+    {
+        p.species = txt;
+    }//End of setpetspecies
+    public static void setpetname (pet p, String txt)
+    {
+        p.name = txt;
+    }//End of setpetname
+    public static String getname (pet p)
+    {
+        return p.name;
+    }//End of getname
+    public static String getspecies (pet p)
+    {
+        return p.species;
+    }//End fo getspecies
+    public static int gethunger (pet p)
+    {
+        return p.hunger;
+    }//End of gethunger
+    public static int gethappiness (pet p)
+    {
+        return p.happiness;
+    }//End of gethappiness
+    public static void setpethappiness (pet p, int newvalue)
+    {
+        p.happiness = newvalue;
+        return;
+    }//End of setpethappiness
+    public static void setpethunger (pet p, int newvalue)
+    {
+        p.hunger = newvalue;
+        return;
+    }//End of setpethunger
 
 }
-
+/* ***************************************************
+ Create a new type (a ADT) called pet that records a pets data
+ A Pet, has a Strign name and species and integer hunger and happiness values
+ Used to store pet details, so the data is used in conjuction with accessor methods, which directly access the attributes
+ Accessor methods:
+ setpetspecies, setpetname, setpethunger, setpethappiness, getname, getspecies, gethunger, gethappiness
+ */
 class pet
 {
     String name;
